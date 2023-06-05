ef4814e98440904153fd02e62eb55f1ff20e1fc2
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-06-04 21:19:15 -0300

    Agora temos que testar

--- TuringMachine.py
+++ TuringMachine.py
@@ -104,13 +104,18 @@ def fazer_trilhas():
     global palavra
     global trilhas
 
+    VALOR = 444 # TODO: Ver quantos brancos devem ser inseridos com o ANDREI
+
     palavra_lista = list(palavra) ## Lista de caracteres da palavra de entrada
     palavra_lista.insert(0, simbolo_inicio) ## Coloca o símbolo de ínicio no primeiro índice
+
+    # Adicionar muitos brancos
+    palavra_lista.extend(simbolo_vazio for _ in range(VALOR))
 
     trilhas.append(palavra_lista)
 
     if qnt_trilhas > 1:
-        trilha_branco = [simbolo_vazio for _ in range(len(palavra_lista))]
+        trilha_branco = [simbolo_vazio for _ in range(len(palavra_lista))] # Mesmo tamanho de palavra_lista
 
         trilhas.extend(trilha_branco for _ in range(qnt_trilhas - 1))
      
@@ -118,9 +123,34 @@ def verificar_trilhas():
     for trilha in trilhas:
         print(trilha)
 
-def checar_transicao(estado : int, cabecote : int) -> list: # Caso retorne lista vazia, não há transição. Caso contrário, retorna transicao
+def checar_transicao(estado, cabecote : int) -> list: # Caso retorne lista vazia, não há transição. Caso contrário, retorna transicao
     global transicoes
-    # TODO:
+    global simbolo_inicio
+    global simbolo_vazio
+    global trilhas
+
+    transicoes_validas_lista = transicoes[estado] # Todas as transições de 'estado'
+
+    for transicao_valida in transicoes_validas_lista:
+        simbolos_leitura = transicao_valida[:qnt_trilhas]
+
+        possivel = True
+
+        for i in range(qnt_trilhas): # Todos devem dar certo
+            if trilhas[i][cabecote] != simbolos_leitura[i]:
+                possivel = False
+                break
+        
+        if possivel:
+            return transicao_valida
+
+
+        # print(f'Novo estado: {novo_estado}\nSimbolos de leitura: {simbolos_leitura}\nSimbolos escrita: {simbolos_escrita}\n')
+
+    return None
+
+def isFinalState(estado) -> bool:
+    return estados[estado][1]
 
 def executar_maquina() -> bool: # Retornar se a palavra faz parte da linguagem ou não
     global estados
@@ -136,10 +166,41 @@ def executar_maquina() -> bool: # Retornar se a palavra faz parte da linguagem o
     global simbolo_inicio
     global simbolo_vazio
 
+    global palavra
+
     cabecote = 1 # Começa no índice 1 (Um depois do símbolo de início)
     estado_atual = estado_inicial
 
     # TODO: Enquanto há transição...
+
+    while True:
+        transicao = checar_transicao(estado_atual, cabecote)
+
+        if transicao is None:
+            break
+
+        novo_estado = transicao[qnt_trilhas] # Pois indexa no 0
+        simbolos_escrita = transicao[qnt_trilhas + 1:-1]
+        mov = transicao[-1] # Ultimo elemento da lista
+
+        # Fazer transicao
+        if mov == '<' and cabecote == 0:
+            break
+        
+        estado_atual = novo_estado
+
+        indice_simbolo_escrita = 0
+
+        for trilha in trilhas:
+            trilha[cabecote] = simbolos_escrita[indice_simbolo_escrita]
+            indice_simbolo_escrita += 1
+
+        if mov == '>': # Direita
+            cabecote += 1
+        else:
+            cabecote -= 1
+
+    return isFinalState(estado_atual)
 
 
 argumentos = sys.argv ## Primeiro argumento é o nome do programa python3
@@ -157,7 +218,7 @@ separar_elementos()
 atribuir_numero_estado()
 
 # verificar_estados()
-print()
+# print()
 
 colocar_transicoes_hash()
 # verificar_transicoes()
@@ -167,9 +228,9 @@ fazer_trilhas()
 # verificar_trilhas()
 
 
-# resposta = executar_maquina()
+resposta = executar_maquina()
 
-# if resposta:
-#     print("Sim")
-# else:
-#     print("Não")
+if resposta:
+    print("Sim")
+else:
+    print("Não")
